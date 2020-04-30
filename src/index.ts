b9f41f11d4cf6580bcee07b53ba55eb4fce6823a
Rodrigo Pinheiro <rpinheiroalmeida@gmail.com>
2020-04-30 20:43:03 -0300

    Removing others providers

--- src/index.ts
+++ src/index.ts
@@ -5,27 +5,17 @@ import { Event } from './model/event';
 import { Message } from './model/message';
 import { DynamodbProvider } from './provider/dynamodb';
 import { InMemoryProvider } from './provider/memory';
-import { MongoProvider } from './provider/mongo';
-import { MySQLProvider } from './provider/mysql';
 import { PersistenceProvider } from './provider/provider';
-import { RedisProvider } from './provider/redis';
+
 import { InMemoryPublisher } from './publisher/memory';
 import { Publisher } from './publisher/publisher';
-import { RabbitMQPublisher } from './publisher/rabbitmq';
-import { RedisPublisher } from './publisher/redis';
 import { SQSPublisher } from './publisher/sqs';
-
 
 export { InMemoryProvider };
 export { PersistenceProvider };
-export { RedisProvider };
-export { MongoProvider };
-export { MySQLProvider };
 export { DynamodbProvider };
 export { Publisher };
 export { InMemoryPublisher };
-export { RabbitMQPublisher };
-export { RedisPublisher };
 export { EventStore };
 export { EventStream };
 export { Event };
