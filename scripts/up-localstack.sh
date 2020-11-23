d3ca8b4fd37ca1d0596a68dbe2b20c34c0541d77
Rodrigo Pinheiro <rpinheiroalmeida@gmail.com>
2020-11-23 12:51:42 -0300

    Adding AWS default region

--- scripts/up-localstack.sh
+++ scripts/up-localstack.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-AWS_DEFAULT_REGION=us-east-1
+export AWS_DEFAULT_REGION=us-east-1
 
 docker-compose -f ./test/integration/docker-compose.yml up -d
 sh ./scripts/sns.sh
