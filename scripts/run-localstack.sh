c7d17c68432a93f41e995a995be91c6a8ab80fd6
Rodrigo Pinheiro <rpinheiroalmeida@gmail.com>
2020-11-23 11:46:35 -0300

    Adding aws credentials

--- scripts/run-localstack.sh
+++ scripts/run-localstack.sh
@@ -1,3 +1,3 @@
-export AWS_ACCESS_KEY_ID=awseventstream
-export AWS_SECRET_ACCESS_KEY=awseventstream
+export AWS_ACCESS_KEY_ID="awseventstream"
+export AWS_SECRET_ACCESS_KEY="awseventstream"
 docker-compose -f ./test/integration/docker-compose.yml up -d
