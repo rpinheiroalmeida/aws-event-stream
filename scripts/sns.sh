20d243d0c9144b450a146a6b6ab6e70fb684e87b
Rodrigo Pinheiro <rpinheiroalmeida@gmail.com>
2020-11-23 13:25:01 -0300

    Debuging topic creation

--- scripts/sns.sh
+++ scripts/sns.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
+echo 'listing topics:'
+aws sns --endpoint-url=http://localhost:4566 list-topics
 echo 'Creating topic...'
-aws sns  --endpoint-url=http://localhost:4566 create-topic --name events
+aws sns --endpoint-url=http://localhost:4566 create-topic --name events
