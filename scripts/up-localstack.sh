722a741776f231e5d081cb1d272d68c4624f5010
Rodrigo Pinheiro <rpinheiroalmeida@gmail.com>
2020-11-23 13:14:47 -0300

    Adding aws credentials

--- scripts/up-localstack.sh
+++ scripts/up-localstack.sh
@@ -2,6 +2,8 @@
 set -e
 
 export AWS_DEFAULT_REGION=us-east-1
+export AWS_ACCESS_KEY_ID="123456-localstack"
+export AWS_SECRET_ACCESS_KEY="123456-localstack"
 
 docker-compose -f ./test/integration/docker-compose.yml up -d
 sh ./scripts/sns.sh
