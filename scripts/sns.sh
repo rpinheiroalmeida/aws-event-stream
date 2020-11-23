82095a579dd6c42b03da79bea62e1c8334441d35
Rodrigo Pinheiro <rpinheiroalmeida@gmail.com>
2020-11-23 13:17:56 -0300

    Adding message

--- scripts/sns.sh
+++ scripts/sns.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 set -e
 
-aws sns  --endpoint-url=http://localhost:4566 create-topic --name events
+echo 'Creating topic...'
+aws sns  --endpoint-url=http://localhost:4566 create-top    ic --name events
