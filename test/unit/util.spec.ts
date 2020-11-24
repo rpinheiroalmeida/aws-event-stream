8a55000e625ea82a9886cc45ad64b0e7214c2ead
Rodrigo Pinheiro <rpinheiroalmeida@gmail.com>
2020-11-24 10:14:51 -0300

    Adding unit test for util

---
+++ test/unit/util.spec.ts
@@ -0,0 +1,17 @@
+import { getEndpointUrl } from '../../src/util';
+
+describe('util', () => {
+    describe('getEndpointUrl', () => {
+        it('should return localhost when NODE_ENV is test', () => {
+            process.env.NODE_ENV = 'test';
+
+            expect(getEndpointUrl()).toEqual({ endpoint: 'http://localhost:4566' });
+        });
+
+        it('should return undefined when NODE_ENV is prd', () => {
+            process.env.NODE_ENV = 'prd';
+
+            expect(getEndpointUrl()).toBeUndefined();
+        });
+    });
+});
