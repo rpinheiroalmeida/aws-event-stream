aa0538a5e42e0a454c41b836c49ea4d2ed1e15ad
Rodrigo Pinheiro <rpinheiroalmeida@gmail.com>
2022-05-03 12:03:54 -0300

    Removing TTY command

--- test/integration/fixture/teardown.sh
+++ test/integration/fixture/teardown.sh
@@ -9,7 +9,7 @@ end=$'\e[0m'
 CURRENT_DIR=$(basename $PWD)
 
 echo "${yel}Inicializando Terraform para destruição de recursos utilizados pela aplicação no Localstack${end}"
-docker container run --rm -it -v $PWD/test/integration/fixture/terraform:/app -w /app --entrypoint '' --link localstack_main --env TF_VAR_HOST=localstack_main --network "$CURRENT_DIR"_integration_test_network hashicorp/terraform:light terraform destroy -lock=false -auto-approve
+docker container run --rm -v $PWD/test/integration/fixture/terraform:/app -w /app --entrypoint '' --link localstack_main --env TF_VAR_HOST=localstack_main --network "$CURRENT_DIR"_integration_test_network hashicorp/terraform:light terraform destroy -lock=false -auto-approve
 
 echo "${yel}Inicializando destruição dos containers do Localstack e da aplicação${end}"
 docker-compose -f test/integration/fixture/docker-compose.yml --project-directory . down
